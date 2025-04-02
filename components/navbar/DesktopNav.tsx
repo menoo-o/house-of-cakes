f5d9bdff7759052b1aa2ac3e4356fb362147749f
menoo-o <menosuper6@gmail.com>
2025-04-02 15:57:30 +0500

    adding navbar

--- components/navbar/DesktopNav.tsx
+++ components/navbar/DesktopNav.tsx
@@ -10,17 +10,17 @@ export default function DesktopNav() {
          width={90} 
          height={90}    
          quality={100}  // Increase quality
-         className="scale-100 md:scale-125 lg:scale-250 xl:scale-290 2xl:scale-320 object-contain p-1"
+         className="scale-100 md:scale-125 lg:scale-220 xl:scale-290 2xl:scale-320 object-contain p-1"
     />
 
       <input type="text" placeholder="Search..."  className="w-1/3 p-2 border rounded-md focus:outline-none" />
 
       <div className="flex items-center space-x-6">
         <Link href="/cart">
-          <Image src="/icons/cart.svg" alt="Cart" width={30} height={30} />
+          <Image src="/icons/basket.svg" alt="Cart" width={28} height={28} />
         </Link>
         <Link href="/account/login">
-          <Image src="/icons/login.svg" alt="Login" width={30} height={30} />
+          <Image src="/icons/personn.svg" alt="Login" width={30} height={30} />
         </Link>
       </div>
     </nav>
--- components/navbar/MobileNav.tsx
+++ components/navbar/MobileNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Sheet, SheetTrigger, SheetContent, SheetTitle } from "@/components/ui/sheet";
+import { Sheet, SheetTrigger, SheetContent, SheetTitle, SheetClose } from "@/components/ui/sheet";
 import Link from "next/link";
 import Image from "next/image";
 import { Menu } from "lucide-react";
@@ -11,19 +11,54 @@ export default function MobileNav() {
       {/* Hamburger Menu */}
       <Sheet>
         <SheetTrigger>
-          <Menu className="h-6 w-6" />
+          <Menu className="h-8 w-8 " />
         </SheetTrigger>
 
         <SheetTitle className="hidden">
         </SheetTitle> 
 
-        <SheetContent side="left" className="w-[315px]">
-          <ul className="space-y-4 text-lg">
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/shop">Shop</Link></li>
-            <li><Link href="/account/login">Login</Link></li>
-          </ul>
-        </SheetContent>
+        <SheetContent side="left" className="w-[315px] bg-white shadow-lg p-5 flex flex-col h-full">
+
+   
+            {/* Account Section */}
+            <div className="mt-4 mb-6">
+                <Link href="/account" className="flex items-center gap-3 text-lg font-medium text-gray-800 hover:text-pink-600 transition">
+                Account <Image src="/icons/personn.svg" alt="Account" width={24} height={24} />
+                </Link>
+            </div>
+
+            {/* Main Navigation */}
+            <nav>
+                <ul className="space-y-4 text-lg font-medium text-gray-800">
+                <li><Link href="/" className="hover:text-pink-600 transition">Easter</Link></li>
+                <li><Link href="/cakes" className="hover:text-pink-600 transition">Cakes</Link></li>
+                <li><Link href="/cupcakes" className="hover:text-pink-600 transition">Cupcakes</Link></li>
+                <li><Link href="/biscuits" className="hover:text-pink-600 transition">Biscuits</Link></li>
+                <li><Link href="/gifts" className="hover:text-pink-600 transition">Gifts</Link></li>
+                </ul>
+            </nav>
+
+            {/* Additional Info */}
+            <div className="mt-10 border-t pt-5">
+                <ul className="space-y-2 text-sm text-gray-600">
+                <li><Link href="/contact" className="hover:text-pink-600 transition">Contact Us</Link></li>
+                <li><Link href="/order-info" className="hover:text-pink-600 transition">Order Info</Link></li>
+                <li><Link href="/product-care" className="hover:text-pink-600 transition">Product Care Info</Link></li>
+                </ul>
+            </div>
+
+            {/* Social Icons - Placed Below Product Info */}
+            <div className="mt-12 flex items-center gap-7 justify-center border-t pt-6">
+                <Link href="https://instagram.com" className="hover:scale-110 transition">
+                <Image src="/icons/insta.svg" alt="Instagram" width={25} height={25} />
+                </Link>
+                <Link href="https://tiktok.com" className="hover:scale-110 transition">
+                <Image src="/icons/tiktok.svg" alt="TikTok" width={25} height={25} />
+                </Link>
+            </div>
+</SheetContent>
+
+
 
       </Sheet>
 
